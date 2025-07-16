3ef749463b865a2fde4445c512f142b4d70ef84e
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-07-16 21:15:42 +0800

    cooking some shit

--- public/js/main.js
+++ public/js/main.js
@@ -201,7 +201,10 @@ function renderStrokes(canvas, ctx, strokes, clearCanvas = false) {
 function zoomIn(canvas) {
     zoomLevel = Math.min(zoomLevel + ZOOM_STEP, MAX_ZOOM);
     localStorage.setItem('canvasZoomLevel', zoomLevel);
+
+    centerCanvas(canvas);
     loadCanvasPosition();
+
     applyZoom(canvas);
     updateZoomButtons();
 }
@@ -209,7 +212,10 @@ function zoomIn(canvas) {
 function zoomOut(canvas) {
     zoomLevel = Math.round(Math.max(zoomLevel - ZOOM_STEP, MIN_ZOOM) * 10) / 10;
     localStorage.setItem('canvasZoomLevel', zoomLevel);
+
+    centerCanvas(canvas);
     loadCanvasPosition();
+
     applyZoom(canvas);
     updateZoomButtons();
 }
