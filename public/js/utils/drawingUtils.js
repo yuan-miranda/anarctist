8573e14d9bab6a8478ec6da16167cd10fab49537
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-09-13 14:53:14 +0800

    500 > 200

--- public/js/utils/drawingUtils.js
+++ public/js/utils/drawingUtils.js
@@ -83,7 +83,7 @@ function getViewportBoundingBox(stage) {
 export async function loadStrokesFromDB(stage, pageGroup) {
     try {
         const viewport = getViewportBoundingBox(stage);
-        const loadPadding = 500;
+        const loadPadding = 200;
 
         const params = new URLSearchParams({
             minX: viewport.x - loadPadding,
